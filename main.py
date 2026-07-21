3c8e8a1e421bdc6665c88381e31f642df6b24c0a
tranvanlong1235321-del <tranvanlong1235321@gmail.com>
2026-07-21 07:23:48 +0000

    connect handler

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
-from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes
+from telegram.ext import Application, CommandHandler
+
+from handlers import start
 
 TOKEN = "7660148947:AAGH2_XFE9Ntpj9NNzr7PacO0o-GRMONvKU"
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(
-        "🚀 Chào mừng đến với NasaCore!\n\n"
-        "Bạn đã trở thành một Chỉ huy Không gian.\n"
-        "Hãy sẵn sàng xây dựng căn cứ đầu tiên!"
-    )
 
 def main():
     app = Application.builder().token(TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
 
-    print("NasaCore Bot đang chạy...")
+    print("🚀 NasaCore đang chạy...")
+
     app.run_polling()
+
 
 if __name__ == "__main__":
     main()
